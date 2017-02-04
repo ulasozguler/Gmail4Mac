6b3526647472eb9bbf1c48f5692358c975447a93
Ulaş Özgüler <ulasozguler@gmail.com>
2017-02-04 23:12:22 +0300

    fixed params for latest electron-packager version.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-electron-packager . "Gmail4Mac" --platform=darwin --arch=x64 --version=0.35.0 --icon=./icon.icns --overwrite --asar=true --prune=true --out=./compiled
+electron-packager . "Gmail4Mac" --platform=darwin --arch=x64 --electron-version=0.35.0 --icon=./icon.icns --overwrite --prune=true --out=./compiled
